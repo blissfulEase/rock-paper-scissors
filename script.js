0124ec63fdace3e26dace844dfe061bd7747ea11
Zair Mejias <133295447+blissfulEase@users.noreply.github.com>
2024-01-27 18:18:31 -0500

    feat: Add single round function
    The function takes the player case-insensitive
    selection, compares against computer selection
    and returns the win/lose result.

--- script.js
+++ script.js
@@ -8,18 +8,37 @@ Output a string that declares the winner of the round
 If TIE, round re-starts  */
 
 function getComputerChoice() {
-    let rock = 1;
-    let scissors = 2;
-    let paper = 3;
+  let rock = 1;
+  let scissors = 2;
+  let paper = 3;
 
-    let selection = Math.floor(Math.random() * 3) + 1;
+  let selection = Math.floor(Math.random() * 3) + 1;
 
-    if (selection === rock) {
-        return "rock";
-    } else if (selection === scissors){
-        return "scissors";
-    } else if (selection === paper) {
-        return "paper";
-    }
+  if (selection === rock) {
+    return "rock";
+  } else if (selection === scissors) {
+    return "scissors";
+  } else if (selection === paper) {
+    return "paper";
+  }
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return "It's a TIE!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) { return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  } else {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  }
+}
+
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
 
